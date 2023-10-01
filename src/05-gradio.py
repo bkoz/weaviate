de3d401ec6e42d2a009c666a814d212de75d4a5c
Bob Kozdemba <bkozdemba@gmail.com>
2023-10-01 17:08:24 +0000

    parsed response to a return string

--- src/05-gradio.py
+++ src/05-gradio.py
@@ -53,16 +53,16 @@ def generative_search(concept: str, prompt: str) -> str:
         .get("Question", ["question", "answer", "category"])
         .with_near_text({"concepts": [concept]})
         .with_generate(single_prompt=prompt)
-        .with_limit(2)
+        .with_limit(1)
         .do()
     )
 
     logging.info('==> OpenAI query')
     logging.info(json.dumps(response, indent=4))
     output = json.dumps(response, indent=4)
-    return json.dumps(response, indent=4)
-
-gr.Interface(fn=generative_search, inputs=["text", "text"], outputs="text",
+    result = response.get('data')['Get']['Question'][0]['_additional']['generate']['singleResult']
+    return result
+gr.Interface(max_lines=20, fn=generative_search, inputs=["text", "text"], outputs="text",
     examples=[["biology", "Explain {answer} as you might to a five-year-old."],
               ["biology", "Explain {answer} as you might to a graduate student."]])\
     .launch()
