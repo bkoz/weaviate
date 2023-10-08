5c99f0f6e9fb0046d79339919b3a8b81cdb1b3d0
Bob Kozdemba <bkozdemba@gmail.com>
2023-10-08 16:16:06 +0000

    Logging individual node status

--- src/00-test-connection.py
+++ src/00-test-connection.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     client = weaviate_utils.weaviate_connection()
 
-    logging.info('')
     if client.is_ready():
-        logging.info('get_nodes_status(): %s', client.cluster.get_nodes_status())
         logging.info('')
+        for node in client.cluster.get_nodes_status():
+            logging.info(node)
+            logging.info('')
